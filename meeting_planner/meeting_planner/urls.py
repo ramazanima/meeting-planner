c381a3b2140e4998a7a98044f6efb5cb52b9c46f
Ali Ramazani <ramazanim@berea.edu>
2023-06-03 17:39:47 -0400

    added the Meeting model for db

--- meeting_planner/meeting_planner/urls.py
+++ meeting_planner/meeting_planner/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from website.views import welcome
+from website.views import welcome, message
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('welcome.html', welcome)
+    path('welcome.html', welcome),
+    path('', message)
 ]
--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 
 def welcome(request):
     return HttpResponse("Welcome to the Meeting Planner!")
-
+def message(request):
+    return HttpResponse("I'm Ali Ramazani")
