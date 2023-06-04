10310879485fcc927e0d2bbe9f345883f8a87832
Ali Ramazani <ramazanim@berea.edu>
2023-06-04 09:19:08 -0400

    combined model, view, and template; added dynamic webpage

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from meetings.models import Meeting
 
 def welcome(request):
     return HttpResponse("Welcome to the Meeting Planner!")
 def message(request):
-    return HttpResponse("I'm Ali Ramazani")
+    return render(request, "website/welcome.html",
+                  {"num_meetings": Meeting.objects.count()})
